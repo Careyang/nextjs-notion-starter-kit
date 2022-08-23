7378f07410f48f9ff5f0c381330e6cc310986b6d
careyang <mastercy919@hotmail.com>
2022-08-24 00:11:05 +0800

    更新模板

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '2977b10c438f4deb8505acf6815ef076',
+  rootNotionPageId: 'e7edb5650cd043d281ed59064cc94cda',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
+  name: '博客笔记',
   domain: 'www.cysolo.com',
-  author: 'Chen Yang',
+  author: '陈杨',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
