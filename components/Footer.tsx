dead7e00275aedc713d798687ea48507bf7536ff
careyang <mastercy919@hotmail.com>
2022-12-07 17:19:08 +0800

    no message

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -33,7 +33,7 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright 2022 <a href="http://beian.miit.gov.cn">京ICP备20004442号-1</a> {config.author}</div>
+      <div className={styles.copyright}>Copyright 2022 {config.author}  <a href="http://beian.miit.gov.cn">京ICP备20004442号-1</a> </div>
 
       <div className={styles.settings}>
         {hasMounted && (
